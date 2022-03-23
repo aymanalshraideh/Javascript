9467cf427306f9be3f87ef271d1cdb7b005ce455
Ayman Al-Shraiedeh <aymanshraideh96@gmail.com>
2022-03-23 18:13:13 +0300

    main

--- Javascript_Tasks/JS-21-march-2022/Task3/Task.js
+++ Javascript_Tasks/JS-21-march-2022/Task3/Task.js
@@ -72,5 +72,187 @@ Ex: repeatStr("to",4); => "to to to to"
 */
 
 /******* Start Your Code *********/
+// function repeatStr(string,number){
+// var re="";
+// while(number>0){
+
+//     re+=string+" ";
+//     number--;
+
+
+// }
+
+
+// return re;
+
+// }
+// console.log(repeatStr("ayman",3));
+
+
+/******* End Your Code ********* */
+
+
+
+/*4-
+Create a function called sum2
+that take two parameter
+and will return the sumation from the first number to the second number
+Ex: sum2(4, 5); => 4 + 5 => 9
+Ex: sum2(3, 6); => 3 + 4 + 5 + 6 => 18
+*/
+/******* Start Your Code *********/
+// function sum2(num1,num2){
+// let m=num1;
+// let n=num2;
+// let i=m-1;
+// if (m>n)
+// {
+// while(i>=n){
+// m=m+i;
+// i--;
+
+
+// }
+
+// }
+// return m;
+
+
+// }
+// console.log(sum2(6,3));
+
+/******* End Your Code ********* */
+
+
+
+
+
+/*
+5
+Create a function called repeatStr2
+that takes 2 string parameters,
+and return the first string number time
+(of the lenght of the secend string) with space betweeen it
+** Important: the continue condition should be [somthing!== 0]
+** Important: You should't have extra space at the end
+Ex: repeatStr2("ro","cc"); => "ro ro"
+Ex: repeatStr2("ro","fff"); => "ro ro ro"
+*/
+
+/******* Start Your Code *********/
+// function repeatStr(string,string2){
+//     var re="";
+    
+// var n=string2.length;
+
+//     while(0<n){
+    
+//         re+=string+" ";
+//         n--;
+    
+    
+//     }
+    
+    
+//     return re;
+    
+//     }
+//     console.log(repeatStr("ayman","ssds"));
+    
+
+
+
+/******* End Your Code ********* */
+
+
+
+/*
+6
+Create a function called multiOf
+that takes 3 parameters,
+and return the first number mutiple by
+(the second one ^ the third number)
+** Important: dont use **
+Ex: multiOf(4,10,3); => 4000
+Ex: multiOf(6,3,2); => 54
+Ex: multiOf(6,2,3); => 48
+*/
+
+/******* Start Your Code *********/
+// function multiOf(num1,num2,num3){
+
+// let index =1;
+
+// while(num3>0){
+// index=index*num2;
+// num3--;
+
+
+// }
+
+// return index*num1;
+
+// }
+// console.log(multiOf(6,3,2));
+
+
+/******* End Your Code ********* */
+
+
+
+/*
+7
+Create a function called muti2
+that take two parameter
+and will return the multiplication
+from the first number to the second number
+Ex: muti2(4, 5); => 4 * 5 => 20
+Ex: muti2(3, 6); => 3 * 4 * 5 * 6 => 360
+*/
+/******* Start Your Code *********/
+
+// function sum2(num1,num2){
+// let m=num1;
+// let n=num2;
+// let i=m-1;
+// if (m>n)
+// {
+// while(i>=n){
+// m=m*i;
+// i--;
+
+
+// }
+
+// }
+// return m;
+
+
+// }
+// console.log(sum2(6,3));
+
+
+
+
+
+/******* End Your Code ********* */
+
+
+
+/*
+8
+Create a function called numberBetweenUs
+that take 2 parameters
+and return the number between them
+** Important: You should't have extra space at the end and comma
+** Important: the stop condition should [num1!==num2-1]
+** hint: maybe you will have 2 if statment
+numberBetweenUs(2,8) => "3, 4, 5, 6, 7"
+numberBetweenUs(1,3) => "2"
+*/
+
+/******* End Your Code ********* */
+
+
 
 /******* End Your Code ********* */
