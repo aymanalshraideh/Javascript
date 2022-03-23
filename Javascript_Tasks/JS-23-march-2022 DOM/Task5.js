c1a7c38e91dc37dfb32c896f0a1a9872f4afb79a
Ayman Al-Shraiedeh <aymanshraideh96@gmail.com>
2022-03-24 02:38:26 +0300

    main

--- Javascript_Tasks/JS-23-march-2022 DOM/Task5.js
+++ Javascript_Tasks/JS-23-march-2022 DOM/Task5.js
@@ -1,19 +1,20 @@
-let l1 =document.getElementById("lab1");
-let l2 =document.getElementById("lab2");
-function password(){
-     let p1=document.getElementById("pass1").value;
-     if (p1.length<6){
-        l1.innerHTML="Too Short";
-        l1.style.color="red";
-     }else{l1.innerHTML="";}
+let l1 = document.getElementById("lab1");
+let l2 = document.getElementById("lab2");
+function password() {
+   let p1 = document.getElementById("pass1").value;
+   if (p1.length < 6) {
+      l1.innerHTML = "Too Short";
+      l1.style.color = "red";
+   } else { l1.innerHTML = ""; }
 }
-function password2(){
-    let p2=document.getElementById("pass2").value;
-    let p1=document.getElementById("pass1").value;
-    if (p2!=p1){
-       l2.innerHTML="The Two Password don't match";
-       l2.style.color="red";
-    }else{l2.innerHTML="";
-document.getElementById("sub").style.display="block";
-}
+function password2() {
+   let p2 = document.getElementById("pass2").value;
+   let p1 = document.getElementById("pass1").value;
+   if (p2 != p1) {
+      l2.innerHTML = "The Two Password don't match";
+      l2.style.color = "red";
+   } else {
+      l2.innerHTML = "";
+      document.getElementById("sub").style.display = "block";
+   }
 }
