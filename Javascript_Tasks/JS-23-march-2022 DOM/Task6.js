e770263d2cc1ab4de08369f0ee07ae6296b3c258
Ayman Al-Shraiedeh <aymanshraideh96@gmail.com>
2022-03-24 20:23:05 +0300

    main

---
+++ Javascript_Tasks/JS-23-march-2022 DOM/Task6.js
@@ -0,0 +1,12 @@
+let p=document.getElementsByClassName("pp");
+let t=document.getElementById("ts").value;
+let i=document.getElementById("in");
+ 
+function btn(){
+if(i==true){
+p[0].setAttribute("t")
+
+}
+
+
+}
